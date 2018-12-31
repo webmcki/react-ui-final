5cf8ade8c3869701aad99e3deface29d30fc8d48
webmcki <webmcki@gmail.com>
2018-12-31 17:20:23 +0900

    key값 찾기

--- src/store.js
+++ src/store.js
@@ -2,31 +2,31 @@ export const muscles = ['어깨', '가슴', '팔', '등', '다리']
 
 export const exercises = [
   {
-    id: '아이디1',
+    id: 1,
     title: '타이틀1',
     description: '설명1',
     muscles: '어깨'
   },
   {
-    id: '아이디1',
+    id: 2,
     title: '타이틀2',
     description: '설명1',
     muscles: '가슴'
   },
   {
-    id: '아이디1',
+    id: 3,
     title: '타이틀3',
     description: '설명1',
     muscles: '팔'
   },
   {
-    id: '아이디1',
+    id: 4,
     title: '타이틀4',
     description: '설명1',
     muscles: '등'
   },
   {
-    id: '아이디1',
+    id: 5,
     title: '타이틀5',
     description: '설명1',
     muscles: '다리'
