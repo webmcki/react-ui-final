6ee625e18416f3704a7c910e3c2fb75e607a93f4
webmcki <webmcki@gmail.com>
2018-12-31 13:26:32 +0900

    스니펫 수정

---
+++ src/cpnts/App.js
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>App</h1>
+      </Fragment>
+    )
+  }
+}
